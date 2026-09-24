bd3f47b6c315f2aba6c47638c8be25985a29f309
Paulo Henrique Andrade de Oliveira <rm96804@estudante.fieb.edu.br>
2026-09-24 19:38:08 -0300

    Colocando todos os arquivos do projeto de volta

--- src/main/java/com/belval/academia/AcademiaApplication.java
+++ src/main/java/com/belval/academia/AcademiaApplication.java
@@ -2,10 +2,12 @@ package com.belval.academia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AcademiaApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
         SpringApplication.run(AcademiaApplication.class, args);
     }
 }
